3a8dcd7b0b26c4f391f603700115984126e3838b
AneteZepa <32796909+AneteZepa@users.noreply.github.com>
2017-10-15 08:50:03 +0100

    Update Solution.java

--- JapocalypseServer/src/Solution.java
+++ JapocalypseServer/src/Solution.java
@@ -1,7 +1,7 @@
 import Server.Japocalypse;
 
 /**
- * Created by mpampis on 7/2/2017.
+ * Created by sheffboom on 10/15/2017.
  */
 public class Solution {
     public static void main(String[] args) throws  Exception {
